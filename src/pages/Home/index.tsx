9bf4ada26c993181b5aa5923e32ec972e773a84a
Gotchinomics <gotchinomics@gmail.com>
2021-11-30 22:11:52 +0100

    updated front page with a simple gotchi image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
 //import myGotchi from "assets/images/gotchi_example.png";
 import "./App.css";
+import myGotchi from "assets/images/gotchi_example.png";
 
 const Home = (): JSX.Element => {
   return (
     <header className="App-header">
-      <img
-        src={"assets/images/gotchi_example.png"}
-        className="App-logo"
-        alt="My Gotchi"
-      />
+      <img src={myGotchi} alt="Yoda#3934" width="512" height="512" />
+      Yoda#3934
     </header>
   );
 };
