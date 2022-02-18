ace8c0595d816cac1f4b1f20b0f6ebe01fd517fd
Gotchinomics <gotchinomics@gmail.com>
2022-02-18 21:11:44 +0100

    Added wallet button and dummy toggle funtionality

--- src/components/HUD/Crypto.tsx
+++ src/components/HUD/Crypto.tsx
@@ -2,12 +2,14 @@ import * as Chisel from "chisel-api-interface";
 import Client from "matchmaking/Client";
 import styles from "./styles.module.css";
 import cargoIcon from "assets/hud/cargo_icon.svg";
+import walletIcon from "assets/hud/wallet_icon.png";
 import { useEffect, useState } from "react";
 import { CargoEntry } from "matchmaking/Schemas";
 
 const Crypto = () => {
   //prettier-ignore
   const world: Chisel.DetailedWorld | undefined =   Client.getInstance().chiselWorld;
+  const [walletOn, setWalletOn] = useState(false);
 
   let crystalsArray: {
     cryptoID: number;
@@ -29,8 +31,6 @@ const Crypto = () => {
 
   const [playerCargo, setPlayerCargo] = useState(initialCrystalArray);
 
-  const updateCrystalArray = [];
-
   useEffect(() => {
     //Wait until the player was admitted to the server
     Client.getInstance().phaserGame.events.on("joined_game", () => {
@@ -41,9 +41,9 @@ const Crypto = () => {
             el.quantity = item.amount;
           }
         });
-        //crystalsArray[item.cryptoID].quantity = item.amount;
         setPlayerCargo(crystalsArray);
-        console.log(item.cryptoID);
+
+        // item onCHANGE
         item.onChange = () => {
           crystalsArray.map((el) => {
             if (el.cryptoID == item.cryptoID) {
@@ -52,20 +52,8 @@ const Crypto = () => {
           });
           setPlayerCargo(crystalsArray);
         };
-        item.onRemove = () => {
-          crystalsArray.map((el) => {
-            if (el.cryptoID == item.cryptoID) {
-              el.quantity = 0;
-            }
-          });
-          setPlayerCargo(initialCrystalArray);
-          console.log("item remove callback");
-        };
       };
-      Client.getInstance().ownPlayer.cargo.onRemove = () => {
-        setPlayerCargo(initialCrystalArray);
-        console.log("cargo remove callback");
-      };
+
       // TO DO: Reset cargo after player dies
     });
   }, []);
@@ -93,7 +81,24 @@ const Crypto = () => {
   return (
     <div className={styles.cryptoContainer}>
       <div className={styles.sectionTitle}>CRYPTO</div>
-      <img src={cargoIcon} className={styles.cargoIcon} />
+      <img
+        src={cargoIcon}
+        className={`${styles.cargoIcon} ${
+          walletOn ? styles.iconDeselected : styles.iconSelected
+        }`}
+        onClick={() => {
+          setWalletOn(false);
+        }}
+      />
+      <img
+        src={walletIcon}
+        className={`${styles.walletIcon} ${
+          walletOn ? styles.iconSelected : styles.iconDeselected
+        }`}
+        onClick={() => {
+          setWalletOn(true);
+        }}
+      />
       <div className={styles.cryptoGallery}>{cryptoInventoryList}</div>
     </div>
   );
