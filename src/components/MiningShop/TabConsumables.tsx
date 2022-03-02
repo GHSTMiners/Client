d1855ce694f62f50e03a4bfdd3a9d03c227ab2ec
Gotchinomics <gotchinomics@gmail.com>
2022-03-02 23:11:48 +0100

    Updated shop preview, clickable elements (but dummy functionality)

--- src/components/MiningShop/TabConsumables.tsx
+++ src/components/MiningShop/TabConsumables.tsx
@@ -1,4 +1,4 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, Fragment, useState } from "react";
 import styles from "./styles.module.css"
 import * as Chisel from "chisel-api-interface";
 import Client from "matchmaking/Client";
@@ -8,7 +8,7 @@ const TabConsumables: FC<{}> = () => {
 
   const world: Chisel.DetailedWorld | undefined =   Client.getInstance().chiselWorld;
   
-  type shopItem = { name: string; price: number; image: string };
+  type shopItem = { name: string; price: number; pattern:any; image: string };
 
   let shopItemArray: shopItem[] = [];
 
@@ -17,26 +17,30 @@ const TabConsumables: FC<{}> = () => {
     shopItemArray.push({ 
       name: explosive.name, 
       price: explosive.price, 
+      pattern: explosive.explosion_coordinates,
       image: `https://chisel.gotchiminer.rocks/storage/${explosive.drop_image}`
     })
    })
 
+   const [selectedItem, setSelectedItem] = useState(shopItemArray[0]);
+
+   const displaySelectedItem = (item : shopItem) => {
+    setSelectedItem(item)
+   }
+
    // TO DO: add also consumables when available to the shopItemArray
-   
-  const renderShopItem = (name: string, price: number, image: string) => (
-    <div className={styles.itemContainer}>
-      <img src={image} className={styles.itemImage} />
+  const renderShopItem = (item: shopItem) => (
+  <div className={styles.itemContainer} onClick={()=>{ displaySelectedItem(item); console.log('new click!')}}>
+      <img src={item.image} className={styles.itemImage} />
       <div className={styles.itemText}>
-        {name} : {price} GHST
+        {item.name} : {item.price} GHST
       </div>
-      <button className={styles.buyButton}>
-          BUY
-        </button>
+      <button className={styles.buyButton}> BUY </button>
     </div>
   );
 
   let shopInventory = shopItemArray.map(function (shopItem) {
-    return renderShopItem( shopItem.name, shopItem.price, shopItem.image );
+    return renderShopItem( shopItem );
   });
 
   return (
@@ -46,8 +50,15 @@ const TabConsumables: FC<{}> = () => {
       </div>
       <div className={styles.detailsPanel}>
         <div className={styles.detailsTitle}>
+          {selectedItem.name}
         </div>
         <div className={styles.detailsBody}>
+          <img src={selectedItem.image} className={styles.itemImage} />
+          Price: {selectedItem.price}
+          <div className={styles.amountContainer}>
+            Amount: 1 
+            <button className={styles.buyManyButton}>BUY</button>
+          </div>
         </div>
       </div>
     </div>
