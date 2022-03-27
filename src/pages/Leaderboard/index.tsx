0fcd3c879cebe6f5ffa104fbe020f38960e327e9
Gotchinomics <gotchinomics@gmail.com>
2022-03-27 18:26:45 +0200

    Added gotchi preview to podium

--- src/pages/Leaderboard/index.tsx
+++ src/pages/Leaderboard/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css";
-import { Header } from "components";
+import { GotchiSVG, Header } from "components";
 import gotchiPodium from "assets/svgs/podium.svg"
 import React from "react";
 import LeaderboardTable from "components/LeaderboardTable";
@@ -7,11 +7,30 @@ import LeaderboardFooter from "assets/svgs/leaderboard_footer.svg"
 
 const Leaderboard = (): JSX.Element => {
 
+  const renderGotchi = (id:number)=>{
+    return(
+      <GotchiSVG tokenId={`${id}`}  
+      options={{ animate: true, removeBg: true }}
+      />
+    )
+  }
+
   return (
     <div className={styles.backgroundContainer}>
     <Header />
         <div className={styles.leaderboardContainer}>
-          <img className={styles.gotchiPodium} src={gotchiPodium} />
+          <div className={styles.gotchiPodiumContainer}>
+            <div className={styles.gotchiRank1}>
+              {renderGotchi(20689)}
+            </div>
+            <div className={styles.gotchiRank2}>
+              {renderGotchi(21223)}
+            </div>
+            <div className={styles.gotchiRank3}>
+              {renderGotchi(3934)}
+            </div>
+            <img className={styles.gotchiPodium} src={gotchiPodium} />
+          </div>
           <LeaderboardTable />
           <img className={styles.leaderboardFooter} src={LeaderboardFooter} />      
         </div>
