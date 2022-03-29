80f5ce0b2152dadfae14bbcaead919946c30a300
Gotchinomics <gotchinomics@gmail.com>
2022-03-29 20:15:39 +0200

    updated leaderboard CSS

--- src/pages/Leaderboard/index.tsx
+++ src/pages/Leaderboard/index.tsx
@@ -1,11 +1,33 @@
 import styles from "./styles.module.css";
-import { GotchiSVG, Header } from "components";
+import { GotchiSVG, Header, LeaderboardCoyote } from "components";
 import gotchiPodium from "assets/svgs/podium.svg"
 import React from "react";
 import LeaderboardTable from "components/LeaderboardTable";
 import LeaderboardFooter from "assets/svgs/leaderboard_footer.svg"
+import { useWeb3 } from "web3/context";
+import { HighScore } from "types";
 
 const Leaderboard = (): JSX.Element => {
+
+  const { state: { usersAavegotchis } } = useWeb3();
+  const highScores : Array<HighScore> = [];
+  
+  // Competition parameters 
+  const endDate = new Date('April 15, 2022 16:00:00 UTC+2') ;
+  const getReward = (position : number , score?:  number) => {
+    if (position == 1) {
+      return "1 x MYTHICAL TICKET"
+    } else if (position <= 3 ) {
+      return "1 x LEGENDARY TICKET"
+    } else if (position <= 5 ) {
+      return "1 x RARE TICKET"
+    } else if (position <= 10 ) {
+      return "1 x UNCOMMON TICKET"
+    } else {
+      return ""
+    }
+  }
+  const competition = { endDate , rewards:getReward };
 
   const renderGotchi = (id:number, winner:boolean)=>{
     return(
@@ -46,6 +68,10 @@ const Leaderboard = (): JSX.Element => {
 
           <div className={styles.tableContainer}>
             <LeaderboardTable />
+            {/*<LeaderboardCoyote highscores={highScores} 
+                               ownedGotchis={usersAavegotchis?.map((gotchi) => gotchi.id)}
+                               competition={competition}  
+          />*/}
           </div> 
         </div>
         <img className={styles.leaderboardFooter} src={LeaderboardFooter} /> 
