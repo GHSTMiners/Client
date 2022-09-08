0e5f61aa42faa4dbfa90f6433616a7908f6425ca
Benjamin Martens <benjamin.martens@politie.nl>
2022-09-08 10:39:10 +0200

    Can now make an educated guess on the closest server region

--- src/matchmaking/ServerLocator/index.tsx
+++ src/matchmaking/ServerLocator/index.tsx
@@ -3,14 +3,48 @@ import * as Chisel from "chisel-api-interface";
 
 import axios from 'axios';
 
+export type Point = {
+    lat: number;
+    long: number;
+  };
+
 export default class ServerLocator {
     constructor(apiInterface: Chisel.APIInterface) {
         apiInterface.server_regions().then(regions => {
             this.regions = regions;
+            let nearestLocation : number = Number.MAX_SAFE_INTEGER
             this.estimateClientLocation().then(result => {
+                let clientCoordinate : Point = {lat: result[0], long: result[1]}
+
+                this.regions?.forEach((region : ServerRegion, index : number) => {
+                    let regionCoordinate : Point = {lat: region.latitude as number, long: region.longitude as number}
+                    let distanceToRegion : number = this.distanceBetweenPoints(clientCoordinate, regionCoordinate)
+                    console.log(`Distance to ${region.name}, is ${distanceToRegion/1000} kilometres`)
+                    if(nearestLocation > distanceToRegion) {
+                        nearestLocation = distanceToRegion
+                        this.closestRegion = region
+                    }
+                })
+
+                console.log(`Closest region is: ${this.closestRegion?.name}`)
                 
             })
         })
+    }
+
+    private distanceBetweenPoints(p1 : Point, p2 : Point) {
+        const R = 6371e3; // metres
+        const φ1 = p1.lat * Math.PI/180; // φ, λ in radians
+        const φ2 = p2.lat * Math.PI/180;
+        const Δφ = (p2.lat-p1.lat) * Math.PI/180;
+        const Δλ = (p2.long-p1.long) * Math.PI/180;
+
+        const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return R * c; // in metres
     }
 
     private async estimateClientLocation() : Promise<any> {
@@ -22,4 +56,5 @@ export default class ServerLocator {
     }
 
     private regions? : ServerRegion[]
+    public closestRegion? : ServerRegion
 }
