779f932ef5bb73f2a7239e1edef7514bd859e197
Gotchinomics <gotchinomics@gmail.com>
2022-10-27 21:46:53 +0200

    Added top score and stats css code

--- src/pages/EndGame/components/StatisticTabs/index.tsx
+++ src/pages/EndGame/components/StatisticTabs/index.tsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
+import starIcon from "assets/svgs/star.svg"
 import styles from "./styles.module.css"
+
+type roomStatsData = { name: string, amount: number};
+const fakeData: roomStatsData[] = [
+    { name: 'Blocks Mined', amount: 986},
+    { name: 'Collected Money', amount: 123456},
+    { name: 'Endgame crypto', amount: 99653},
+    { name: 'Damage taken', amount: 159},
+    { name: 'Amount spent in explosives', amount: 655},
+    { name: 'Amount spent in upgrades', amount: 1450},
+    { name: 'Travelled distance', amount: 123512},
+    { name: 'Fueld consumed', amount: 400},
+]
 
 const StatisticsTabs = () => {
     const [selectedTab, setSelectedTab] = useState<number>(0);
+
     return(
         <>
             <div className={styles.tabHeaderButton}>
@@ -12,28 +26,29 @@ const StatisticsTabs = () => {
                 <button className={`${styles.tabButton} ${styles.lastTabButton} ${selectedTab===1? styles.selectedTab:styles.inactiveTab}`}
                     onClick={()=>{ setSelectedTab(1) }}> GigaChads </button>
             </div>
-            
+                
                 <div className={styles.tabPanel} hidden={selectedTab===0? false:true}>
-                    <div className={styles.statsEntry}>Mined blocks</div>
-                    <div className={styles.statsEntry}>Collected Money</div>
-                    <div className={styles.statsEntry}>Endgame crypto</div>
-                    <div className={styles.statsEntry}>Damage taken</div>
-                    <div className={styles.statsEntry}>Amount spent in explosives</div>
-                    <div className={styles.statsEntry}>Amount spent in upgrades</div>
-                    <div className={styles.statsEntry}>Travelled distance</div>
-                    <div className={styles.statsEntry}>Fueld consumed</div>
+                    {  fakeData.map( entry => {
+                        return(
+                            <>  
+                                <div className={styles.entryWrapper}>
+                                    <div className={styles.statsEntry}>
+                                        <div className={styles.entryName}>{entry.name}</div>
+                                        <div className={styles.entryAmount}>{entry.amount}</div>
+                                    </div>
+                                    <div className={styles.topScoreContainer}>
+                                        <img src={starIcon} style={{height: '2rem'}} alt={'top score!'}/>
+                                        Top score!
+                                    </div>
+                                </div>
+                            </>
+                            )
+                        })
+                    }
                 </div>
                 <div className={styles.tabPanel} hidden={selectedTab===1? false:true}>
-                    <div className={styles.statsEntry}>Chad Mined blocks</div>
-                    <div className={styles.statsEntry}>Chad Collected Money</div>
-                    <div className={styles.statsEntry}>Chad Endgame crypto</div>
-                    <div className={styles.statsEntry}>Chad Damage taken</div>
-                    <div className={styles.statsEntry}>Amount spent in explosives</div>
-                    <div className={styles.statsEntry}>Amount spent in upgrades</div>
-                    <div className={styles.statsEntry}>Travelled distance</div>
-                    <div className={styles.statsEntry}>Fueld consumed</div>
+                    Work in progress...
                 </div>
-            
         </>
     )
 }
