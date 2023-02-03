6de0383ec213ced7febcee96cd203b5506721485
Gotchinomics <gotchinomics@gmail.com>
2023-02-03 23:22:39 +0100

    Added the numeric cooldown displayed on the shortcut button

--- src/game/HUD/Console/components/ButtonCooldown.tsx
+++ src/game/HUD/Console/components/ButtonCooldown.tsx
@@ -14,6 +14,7 @@ const ButtonCooldown : React.FC<Props> = ({deadline, itemCooldown} ) => {
             <div className={styles.cooldownContainer} 
                 style={{background:`conic-gradient(rgba(0, 0, 0, 0) ${cooldownProgress}%,rgba(126,240,159,0.8)  0)`}}
                 hidden={(secondsLeft<=0)}>
+                    {secondsLeft}
             </div>
         </>
     )
